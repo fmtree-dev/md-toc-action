82ceb4fedc3ae32ad265f27eb4245ef69194f3b3
Huakun Shen <huakun.shen@huakunshen.com>
2021-03-09 23:27:41 -0500

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 md_toc_dir=$1
 echo "custom directory: $md_toc_dir"
 python main.py --custom_dir $md_toc_dir
-ls
-ls ./data
+ls -a
+ls -a ./data
 cat ./data/README.md
 echo "::set-output name=success::true"
