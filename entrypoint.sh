2b54b29cdb103d7e54702b62a998d67abf86bd1b
Huakun Shen <huakun.shen@huakunshen.com>
2021-03-10 00:32:06 -0500

    remove debug message

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,4 @@
 md_toc_dir=$1
 echo "custom directory: $md_toc_dir"
 python main.py --custom_dir $md_toc_dir
-cat ./data/OSCP/Exercises/README.md
-cat ./data/OSCP/README.md
-cat ./data/README.md
 echo "::set-output name=success::true"
