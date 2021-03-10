3c15c76c32970a86fc9e945cecb42124d7ffab34
Huakun Shen <huakun.shen@huakunshen.com>
2021-03-09 21:13:27 -0500

    Add main.py and update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
+# Initialize and Print Parameters
 md_toc_dir=$1
 echo "custom directory: $md_toc_dir"
-pydoc fmtree
+python main.py --custom_dir $md_toc_dir
 echo "::set-output name=success::true"
